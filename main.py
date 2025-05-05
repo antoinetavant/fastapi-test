8bb457f45a48b57ef32d8b7c8d1cb5897b924a7f
Antoine Tavant <antoinetavant@hotmail.fr>
2025-05-05 13:28:23 +0000

    error handling

--- main.py
+++ main.py
@@ -1,128 +1,68 @@
-from fastapi import FastAPI, Header
-from pydantic import BaseModel
-
-api = FastAPI(title = "My API", description = "My API description", version = "1.0.0", openapi_tags=[
-        {
-        'name': 'home',
-        'description': 'default functions'
-    },
-    {
-        'name': 'items',
-        'description': 'functions that are used to deal with items'
-    }
-])
-
-users_db = [
-    {
-        'user_id': 1,
-        'name': 'Alice',
-        'subscription': 'free tier'
-    },
-    {
-        'user_id': 2,
-        'name': 'Bob',
-        'subscription': 'premium tier'
-    },
-    {
-        'user_id': 3,
-        'name': 'Clementine',
-        'subscription': 'free tier'
-    }
-]
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+import datetime
 
 
-@api.get("/",
-         name="Hellow World", 
-         tags=['home'])
-def greatings():
-    "Returns an Hello word"
-    return "Bienvenu à mon API"
+api = FastAPI()
+
+data = [1, 2, 3, 4, 5]
+
+@api.get('/data')
+def get_data(index):
+    try:
+        return {
+            'data': data[int(index)]
+        }
+    except ValueError:
+        
+        raise HTTPException(
+            status_code=400,
+            detail='Bad Type'
+        )
+    except IndexError:
+        raise HTTPException(
+            status_code=404,
+            detail='Unknown Index')
 
 
-@api.get("/users", tags=['home', 'items'])
-def get_all_users() -> list[dict]:
-    return users_db
+class MyException(Exception):
+    def __init__(self,
+                 name : str,
+                 date: str):
+        self.name = name
+        self.date = date
 
-@api.get("/users/{userid:int}")
-def get_one_user(userid:int) -> dict:
-    for user in users_db:
-        if user["user_id"] == userid:
-            return user
-    return {}
+@api.exception_handler(MyException)
+def MyExceptionHandler(
+    request: Request,
+    exception: MyException
+    ):
+    return JSONResponse(
+        status_code=418,
+        content={
+            'url': str(request.url),
+            'name': exception.name,
+            'message': 'This error is my own',
+            'date': exception.date
+        }
+    )
 
-@api.get("/users/{userid:int}/name")
-def get_one_user_name(userid:int) -> dict:
-    for user in users_db:
-        if user["user_id"] == userid:
-            return {"name" : user["name"]}
-    return {}
+@api.get('/my_custom_exception')
+def get_my_custom_exception():
+    raise MyException(
+      name='my error',
+      date=str(datetime.datetime.now())
+      )
 
-@api.get("/users/{userid:int}/subscription")
-def get_one_user_subcription(userid:int) -> dict:
-    for user in users_db:
-        if user["user_id"] == userid:
-            return {"subscription" : user["subscription"]}
-    return {}
+responses = {
+    200: {"description": "OK"},
+    404: {"description": "Item not found"},
+    302: {"description": "The item was moved"},
+    403: {"description": "Not enough privileges"},
+}
 
-class User(BaseModel):
-    user_id: int
-    name: str
-    subscription: str
-
-@api.put("/users")
-def put_users(user: User):
-    users_db.append({"user_id": user.user_id,
-                     "name": user.name,
-                     "subscription": user.subscription})
-    return user
-
-@api.post("/users/{userid:int}")
-def update_one_user(userid: int, user: dict) -> dict:
-    user_idx = None
-    for i, user in enumerate(users_db):
-        if user["user_id"] == userid:
-            user_idx = i
-            the_user = user
-    if user_idx is None:
-        return {}
-    
-    the_user = the_user | user
-    users_db[user_idx] = the_user
-    return the_user
-
-@api.delete("/users/{userid:int}")
-def remove_one_user(userid: int):
-    user_idx = None
-    for i, user in enumerate(users_db):
-        if user["user_id"] == userid:
-            user_idx = i
-    if user_idx is None:
-        return {}
-    pop = users_db.pop(user_idx)
-    return {"status": "success",
-            "user deleted": pop}
-
-@api.get('/headers')
-def get_headers(user_agent=Header(None)):
+@api.get('/thing', responses=responses)
+def get_thing():
     return {
-        'User-Agent': user_agent
-    }
-
-class Computer(BaseModel):
-    """A computer object"""
-    computerid: int
-    cpu: str | None = None
-    gpu: str | None = None
-    price: float
-
-@api.put("/computer", name="Create a new computer", tags=['home', 'items'])
-def put_computer(computer: Computer):
-    """Creates a new computer within the database
-    """
-    return computer
-
-@api.get("/cutom", name="Get custom Header")
-def get_content(custom_deader: str = Header(None, description='My own personal header')):
-    return {
-        "Custom-header" : custom_deader
+        'data': 'hello world'
     }
